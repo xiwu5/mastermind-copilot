fd7ba988f49b775c087d3953afbf4048623db822
Xi Wu <youngjoycewu@gmail.com>
2025-10-06 17:52:41 -0700

    DRY code in game.py

--- app/game.py
+++ app/game.py
@@ -8,36 +8,43 @@ def generate_code():
     code = []
     
     # Generate a code of 4 random letters from VALID_LETTERS
-    for _ in range(4):
-        code.append(random.choice(list(VALID_LETTERS)))
+    letters_list = list(VALID_LETTERS)
+    return [random.choice(letters_list) for _ in range(4)]
 
-    return code
+def uppercased_list(char_list):
+    """Convert a list of characters to uppercase."""
+    uppercased_list = []
+    for letter in char_list:
+        uppercased_list.append(str(letter).upper())
+    return uppercased_list
 
 
 def validate_guess(guess):
-    # Exit early if guess is not exactly 4 elements long
-    if len(guess) != 4:
+    # Check for empty list or not a list
+    if not isinstance(guess, list) or len(guess) != 4:
         return False
-    
-    # Convert guess to uppercase for case-insensitive comparison
-    uppercased_guess = []
+    # Check all elements are strings and not None
     for letter in guess:
-        uppercased_guess.append(str(letter).upper())
-
-    # Return False if we find an invalid element of guess
+        if not isinstance(letter, str) or letter is None:
+            return False
+    # Convert guess to uppercase for case-insensitive comparison
+    uppercased_guess = normalize_code(guess)
+    # Check all letters are valid
     for letter in uppercased_guess:
         if letter not in VALID_LETTERS:
             return False
-        
     return True
 
+def normalize_code(code):   
+    """
+    Normalizes the casing for a code by converting 
+    the list of characters to uppercase.
+    """    
+    return [str(letter).upper() for letter in code]
 
 def check_code_guessed(guess, code):
-    # Convert guess to uppercase for case-insensitive comparison
-    uppercased_guess = []
-    for letter in guess:
-        uppercased_guess.append(str(letter).upper())
-
+    # Convert guess and code to uppercase for case-insensitive comparison
+    uppercased_guess = normalize_code(guess)
+    uppercased_code = normalize_code(code)
     # Check if the guess and code are identical (win condition)
-    # The guard clause guarantees the number of guesses is 8 or less
-    return code == uppercased_guess
+    return uppercased_code == uppercased_guess
